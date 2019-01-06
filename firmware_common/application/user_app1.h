47ced6a93286a0f377c5f7046ce6c9c769e051af
SeanTheSheepCS <seankenny6688@gmail.com>
2019-01-05 17:48:03 -0700

    First commit!

--- firmware_common/application/user_app1.h
+++ firmware_common/application/user_app1.h
@@ -28,37 +28,6 @@ Type Definitions
 /**********************************************************************************************************************
 Constants / Definitions
 **********************************************************************************************************************/
-#define COUNTER_LIMIT_MS (u32)2500
-#define ALTERNATING_COLOUR_LIMIT_MS (u32)500
-#define CYCLE_COLOURS_LIMIT_MS (u32)1000
-
-#define E4FREQ_HZ (u32)330
-#define G4FREQ_HZ (u32)392
-#define C4FREQ_HZ (u32)262
-#define D4FREQ_HZ (u32)294
-#define F4FREQ_HZ (u32)349
-
-#define QUARTERNOTETIME_MS (u32)500
-
-/* THE FOLLOWING ARE DEFINITIONS USED FOR THE BETTER VERSION OF JINGLE BOOLS */
-/*
-#define METRONOME (u32) 1000
-
-#define FULL_NOTE (u32)(METRONOME)
-#define HALF_NOTE (u32)(METRONOME/2)
-#define QUARTER_NOTE (u32)(METRONOME/4)
-#define EIGHTH_NOTE (u32)(METRONOME/8)
-#define FN FULL_NOTE
-#define HN HALF_NOTE
-#define QN QUARTER_NOTE
-#define EN EIGHTH_NOTE
-
-#define E4 E4FREQ_HZ
-#define G4 G4FREQ_HZ
-#define C4 C4FREQ_HZ
-#define D4 D4FREQ_HZ
-#define F4 F4FREQ_HZ
-*/
 
 /**********************************************************************************************************************
 Function Declarations
@@ -74,13 +43,6 @@ Function Declarations
 /*--------------------------------------------------------------------------------------------------------------------*/
 void UserApp1Initialize(void);
 void UserApp1RunActiveState(void);
-void FestiveLightsPatternOne(bool reset);
-void AllLedsOff(void);
-void CycleLeds(void);
-bool Jingle(bool reset);
-static bool betterJingle(bool reset);
-bool isTimeUp(bool reset, u32 u32time);
-void testLCD(void);
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
